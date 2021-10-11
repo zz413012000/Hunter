23f7e0c35e154d507dd0fe7340eaf03c23930500
zz413012000 <zz413012000@gmail.com>
2021-10-11 20:01:03 +0800

    build about

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about=()=>{
+    return (
+        <div>
+            <h1>關於我</h1>
+            <p>程式生活一年級生</p>
+        </div>
+    )
+}
+
+
+export default about;
